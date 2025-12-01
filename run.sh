3dcc6e6091c0f9035ca34a81fe548aec510df343
LEEJaeHyeok97 <hazardous10@naver.com>
2025-12-01 13:09:35 +0900

    feat: printf 디버깅 추가

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -xe
+
+# 커널 빌드
+clang -std=c11 -O2 -g3 -Wall -Wextra \
+  --target=riscv32-unknown-elf \
+  -ffreestanding -nostdlib \
+  -fno-stack-protector -fuse-ld=lld \
+  -Wl,-Tkernel.ld -Wl,-Map=kernel.map \
+  -o kernel.elf \
+  kernel.c common.c
+
+# QEMU 실행 (OpenSBI 펌웨어 사용)
+qemu-system-riscv32 -machine virt \
+  -bios opensbi/build/platform/generic/firmware/fw_dynamic.bin \
+  -nographic -serial mon:stdio --no-reboot \
+  -kernel kernel.elf
