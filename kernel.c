fe7210ac43bffea7d2cf0c737f01145eaebf295c
LEEJaeHyeok97 <hazardous10@naver.com>
2025-12-08 15:49:20 +0900

    feat: 메모리 할당기 구현

--- kernel.c
+++ kernel.c
@@ -5,7 +5,18 @@ typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
 
-extern char __bss[], __bss_end[], __stack_top[];
+extern char __bss[], __bss_end[], __stack_top[], __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
 
 void *memset(void *buf, char c, size_t n) {
     uint8_t *p = (uint8_t *) buf;
@@ -132,6 +143,13 @@ void kernel_main(void) {
 
     // .bss 초기화 (원래 있던 코드 유지)
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+    PANIC("booted!");
 
     WRITE_CSR(stvec, (uint32_t) kernel_entry); // new
     __asm__ __volatile__("unimp"); // new
